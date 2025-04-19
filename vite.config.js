ebfb1e7aa2dde0bc0aa79df02e8df33b59bb5eee
Angad Singh <angadkollege@gmail.com>
2025-04-20 03:12:32 +0530

    proxy update

--- vite.config.js
+++ vite.config.js
@@ -10,9 +10,7 @@ export default defineConfig({
   ],
   server: {
     proxy: {
-      "/api": {target:"http://13.50.84.146:3000",
-      changeOrigin: true,
-      secure: false,}
+      "/api": "http://13.50.84.146:3000"
     },
   },
 })
